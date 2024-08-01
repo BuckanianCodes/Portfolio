1246be4d2c2cd7e820c9adc3a9033d075d6fb84a
BuckanianMark <kariukimark115@gmail.com>
2024-08-01 22:24:17 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WelcomeComponent } from "./pages/welcome/welcome.component";
+import { TechnologiesComponent } from './pages/technologies/technologies.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WelcomeComponent,TechnologiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
