9b88f84d2cb740d32e3e7ea2e11a76fe7b2e8fc0
BuckanianMark <kariukimark115@gmail.com>
2024-11-27 15:07:49 +0300

    Adding experience

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { MernStackComponent } from './pages/mern-stack/mern-stack.component';
 import { GraphqlStackComponent } from './pages/graphql-stack/graphql-stack.component';
 import { FirebaseStackComponent } from './pages/firebase-stack/firebase-stack.component';
+import { ExperienceComponent } from './pages/experience/experience.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent},
     {path:'mean',component:MeanStackComponent},
     {path:'mern',component:MernStackComponent},
     {path:'graphql',component:GraphqlStackComponent},
-    {path:'firebase',component:FirebaseStackComponent}
+    {path:'firebase',component:FirebaseStackComponent},
+    {path:'experience',component:ExperienceComponent}
 ];
