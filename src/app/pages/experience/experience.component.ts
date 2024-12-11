1012d4bc5d5c816a82d085a2243d2f24c1c798d5
BuckanianCodes <buckanian@gmail.com>
2024-12-11 11:46:02 +0300

    Adding experience message

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
@@ -7,6 +7,28 @@ import { Component } from '@angular/core';
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.css'
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements OnInit{
+  experiences!:any[]
+
+  ngOnInit(){
+    this.experiences = [
+      {
+        companyname:"The Feasify",
+        startdate:"01/07/2024",
+        enddate:"30/08/2024",
+        employementtype:"contract",
+        link:"",
+        websiteurl:""
+      },
+      {
+        companyname:"JackFruit Finance",
+        startdate:"01/09/2024",
+        enddate:"present",
+        employementtype:"hybrid",
+        link:"",
+        websiteurl:""
+      }
+    ]
+  }
 
 }
